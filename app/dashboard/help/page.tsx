5db0b9ec9aa5c6378fdaa5fbfaa65e63a7965e0a
techprevail-hub <tech@withprevail.com>
2026-05-11 13:31:23 +0530

    Help page

---
+++ app/dashboard/help/page.tsx
@@ -0,0 +1,8 @@
+export default function HelpPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Help Center</h1>
+      <p>This page is under development.</p>
+    </div>
+  );
+}
